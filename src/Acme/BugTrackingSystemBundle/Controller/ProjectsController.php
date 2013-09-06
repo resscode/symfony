9279d2d70b82312d11bd7ce157ad4979f5863ec2
adovgobrod <Anton.Dovgobrod@dataart.com>
2013-09-06 18:19:36 +0300

    first commit

---
+++ src/Acme/BugTrackingSystemBundle/Controller/ProjectsController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\BugTrackingSystemBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProjectsController extends Controller
+{
+}
---
+++ src/Acme/BugTrackingSystemBundle/Tests/Controller/ProjectsControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\BugTrackingSystemBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProjectsControllerTest extends WebTestCase
+{
+}
